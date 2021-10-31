5a89e666f34acb8b3e456dade70488a2a742d1ee
alexshostak98 <alexandr_shostak@mail.ru>
2021-10-31 16:27:12 +0300

    Lesson4_homeworks

---
+++ homeworks/les4/ex3.py
@@ -0,0 +1,15 @@
+"""
+Имеется 2 кортежа с координатами Ферзя и Пешки на шахматной доске
+Определить Бьет ли Ферзь пешку
+координаты хранятся (x, y)
+
+
+
+queen_coordinate = input("Введите координы ферзя (x, y)\n>>")
+pawn_coordinate = input("Введите координы пешки (x, y)\n>>").split(" ")
+for coord_1 in pawn_coordinate:
+    if coord_1.isdigit():
+        coord_1 = int(coord_1)
+    else:
+        print("Ошибка ввода")
+"""
