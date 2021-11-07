05bb7cc1d5561e267bcd324a752c7c74606b1857
alexshostak98 <alexandr_shostak@mail.ru>
2021-11-07 11:39:34 +0300

    tic_tac_toe_ver_0.2.4 (added with)

--- tic_tac_toe/logs.py
+++ tic_tac_toe/logs.py
@@ -9,32 +9,25 @@ from interface import user_interface
 def get_game_num() -> int:
     inc = 1
     try:
-        file = open(LOGS_FILES_NAME["game_num"], "r", encoding="UTF-8")
+        with open(LOGS_FILES_NAME["game_num"], "r", encoding="UTF-8") as file:
+            data = file.read()
+            data = int(data) + inc
+        write_to_file("game_num", str(data), "w")
+        return data
     except FileNotFoundError:
         os.mkdir(LOGS_DIR_NAME)
         write_to_file("game_num", str(inc), "w")
         return inc
-    try:
-        data = file.read()
-        if not data:
-            return inc
-        inc = int(data) + 1
-        return inc
     except IOError:
         print(user_interface("log_error"))
-    finally:
-        file.close()
-        write_to_file("game_num", str(inc), "w")
 
 
 def write_to_file(name: str, message: str, access_type="a"):
-    file = open(LOGS_FILES_NAME[name], access_type, encoding="UTF-8")
     try:
-        file.write(message)
+        with open(LOGS_FILES_NAME[name], access_type, encoding="UTF-8") as file:
+            file.write(message)
     except IOError:
         print(user_interface("log_error"))
-    finally:
-        file.close()
 
 
 def init_log_message(template_name: str, game_num: int, **log_data) -> str:
